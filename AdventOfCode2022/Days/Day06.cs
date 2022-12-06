3d95d1caae793235e37469d072839bd518b31e14
Merarox <nnibr1@gmail.com>
2022-12-06 08:53:00 +0100

    Day06

--- AdventOfCode2022/Days/Day06.cs
+++ AdventOfCode2022/Days/Day06.cs
@@ -11,11 +11,53 @@ namespace AdventOfCode2022.Days
         public void PartOne()
         {
             string[] lines = File.ReadAllLines(@"../../../Inputs/input06.txt");
+            int i = 0;
+            List<char> save = new List<char>();
+            foreach(string line in lines)
+            {
+                foreach(char c in line)
+                {
+                    if (save.Contains(c))
+                    {
+                        int index = save.IndexOf(c);
+                        save.RemoveRange(0, index + 1);
+                    }
+                    else if(save.Count == 4)
+                    {
+                        break;
+                    }
+                    save.Add(c);
+                    i++;
+                }
+            }
+
+            Console.WriteLine($"Index: {i}");
         }
 
         public void PartTwo()
         {
             string[] lines = File.ReadAllLines(@"../../../Inputs/input06.txt");
+            int i = 0;
+            List<char> save = new List<char>();
+            foreach (string line in lines)
+            {
+                foreach (char c in line)
+                {
+                    if (save.Contains(c))
+                    {
+                        int index = save.IndexOf(c);
+                        save.RemoveRange(0, index + 1);
+                    }
+                    else if (save.Count == 14)
+                    {
+                        break;
+                    }
+                    save.Add(c);
+                    i++;
+                }
+            }
+
+            Console.WriteLine($"Index: {i}");
         }
     }
 }
